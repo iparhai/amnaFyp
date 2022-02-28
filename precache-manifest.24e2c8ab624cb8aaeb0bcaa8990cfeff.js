9782424b870fb270018c1f7684365f84c92aa47a
iparhai <iparhai@nu.edu.pk>
2022-02-28 15:02:34 +0500

    Updates

---
+++ precache-manifest.24e2c8ab624cb8aaeb0bcaa8990cfeff.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "f34bad2dba0237398d97",
+    "url": "/amnaFyp/static/css/main.dacb3b3a.chunk.css"
+  },
+  {
+    "revision": "f34bad2dba0237398d97",
+    "url": "/amnaFyp/static/js/main.c5644685.chunk.js"
+  },
+  {
+    "revision": "3cdc18529bc97cb0bdce",
+    "url": "/amnaFyp/static/js/runtime~main.b3886d6f.js"
+  },
+  {
+    "revision": "e2e0e62535051793ec18",
+    "url": "/amnaFyp/static/css/2.3c3c190d.chunk.css"
+  },
+  {
+    "revision": "e2e0e62535051793ec18",
+    "url": "/amnaFyp/static/js/2.50190508.chunk.js"
+  },
+  {
+    "revision": "424d18986561c7039221eb97a66aef6b",
+    "url": "/amnaFyp/static/media/tostartpressbutton.424d1898.mp3"
+  },
+  {
+    "revision": "8efff185f64e4259ae68a4074dbfb0e6",
+    "url": "/amnaFyp/static/media/321go.8efff185.mp3"
+  },
+  {
+    "revision": "09b0b7bb4e8aad40481825ec9d91d223",
+    "url": "/amnaFyp/static/media/rightAnswer.09b0b7bb.mp3"
+  },
+  {
+    "revision": "ddcbc59b2917e3dbeb737fca1e3ede4e",
+    "url": "/amnaFyp/static/media/wrongAnswer.ddcbc59b.mp3"
+  },
+  {
+    "revision": "a0d6b13874a29393b0f8252f2adb5fa7",
+    "url": "/amnaFyp/static/media/grocerySound.a0d6b138.mp3"
+  },
+  {
+    "revision": "efc0e3314bf0a9623a140aa5e147db9f",
+    "url": "/amnaFyp/static/media/background.efc0e331.gif"
+  },
+  {
+    "revision": "669f8cdb6aa1730b188e1d564f50d648",
+    "url": "/amnaFyp/static/media/bar.669f8cdb.png"
+  },
+  {
+    "revision": "3765e23ba97065cce785f6936f0c48db",
+    "url": "/amnaFyp/index.html"
+  }
+];
